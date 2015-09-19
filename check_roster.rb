4bb897ab35ed28eee0383d77f49f32aa73ea2189
Soumya Ray <soumya.ray@gmail.com>
2015-09-19 17:17:13 +0800

    Split into two classes
    - Create new ClassList class
    - Rename old class into ListComparison

--- check_roster.rb
+++ check_roster.rb
@@ -1,15 +1,25 @@
-class ClassLists
-  def initialize(list1, list2)
-    @list1 = split_list(list1)
-    @list2 = split_list(list2)
+class ClassList
+  attr_reader :list
+
+  def initialize(list)
+    @list = split_into_lines(list)
   end
 
-  def split_list(list)
+  private
+
+  def split_into_lines(list)
     list.split("\n")
+  end
+end
+
+class ListComparison
+  def initialize(list1, list2)
+    @list1 = list1
+    @list2 = list2
   end
 
   def compare
-    puts "Newly joined: #{@list2 - @list1}"
-    puts "Dropped out: #{@list1 - @list2}"
+    puts "Newly joined: #{@list2.list - @list1.list}"
+    puts "Dropped out: #{@list1.list - @list2.list}"
   end
 end
